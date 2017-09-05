b27d6af60a7599bba5222d531bdaabfd69cc5470
Vincent <vincent8992@gmail.com>
2017-09-05 12:59:13 +0200

    wrapping printer

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from service.messageService import MessageService
 from network.network_layer import NetworkLayer
-
+from printer.printer import Printer
 
 if __name__ == "__main__":
 
+    printer = Printer(address="123")
     network_layer = NetworkLayer(endpoint="https://booboomessenger.herokuapp.com/")
     service = MessageService(network_layer=network_layer)
 
     all_messages = service.get_messages()
-    service.mark_message_as_printed(all_messages[0])
+
+    for message_to_be_printed in all_messages:
+        printer.print(message_to_be_printed.message)
+        service.mark_message_as_printed(message_to_be_printed)
---
+++ printer/printer.py
@@ -0,0 +1,8 @@
+
+class Printer:
+
+    def __init__(self, address):
+        self.address = address
+
+    def print(self, message):
+        print("printing message: " + message)
